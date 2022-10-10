16d07e0c6f147b90c081da6d44c9af7e655d7118
Imran-Ho <imranhossain36@gmail.com>
2022-10-10 23:32:53 +0600

    route created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Quizs from './components/Quizs/Quizs';
+import Statistics from './components/Quizs/Statistics/Statistics';
+import Main from './layout/Main';
 
 function App() {
   const router = createBrowserRouter([
-
+    {path: '/', element:<Main></Main>, children: [
+      {path: '/', element: <Quizs></Quizs>},
+      {path: '/statistics', element: <Statistics></Statistics>},
+      {path: '/blog', element: <Blog></Blog>},
+    ]}
   ])
   return (
     <div className="App">
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Home from '../components/Home/Home';
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header/Header';
+
 
 const Main = () => {
     return (
         <div>
-            <Home></Home>
+            <Header></Header>
+            <Outlet></Outlet>
             
         </div>
     );
